4cf2fa167c9a63562770a11d6f9dd35c12911764
AndresQuijano <andresquijanoarroyave@gmail.com>
2020-09-30 15:53:40 -0500

    Initial add of the project.

---
+++ src/main/java/guru/springframework/guruspring2petclinic/GuruSpring2PetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.guruspring2petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruSpring2PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuruSpring2PetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/guruspring2petclinic/GuruSpring2PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.guruspring2petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruSpring2PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
