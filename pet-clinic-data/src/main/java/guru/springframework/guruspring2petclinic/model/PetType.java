5ea824066d859f645eb75b8792c31402c096dc70
AndresQuijano <andresquijanoarroyave@gmail.com>
2020-10-14 19:51:19 -0500

    Implements Id to model objects. Closes #10

--- pet-clinic-data/src/main/java/guru/springframework/guruspring2petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/guruspring2petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package guru.springframework.guruspring2petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
