ef684519f96ed7005eb4ace1a3953d9b8b2521c0
AndresQuijano <andresquijanoarroyave@gmail.com>
2020-10-17 16:26:48 -0500

    Lecture 80 done. Create bootstrap data and refactor service map to implement subinterfaces instead of the super interface CrudServiceMap. It was supposed to be corrected on the same lecture where it was made but this guy seems to be mediocre.  Closes #14.

--- pet-clinic-data/src/main/java/guru/springframework/guruspring2petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/guruspring2petclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package guru.springframework.guruspring2petclinic.services.map;
 
 import guru.springframework.guruspring2petclinic.model.Pet;
-import guru.springframework.guruspring2petclinic.services.CrudService;
+import guru.springframework.guruspring2petclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/guru/springframework/guruspring2petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/guruspring2petclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package guru.springframework.guruspring2petclinic.services.map;
 
 import guru.springframework.guruspring2petclinic.model.Vet;
-import guru.springframework.guruspring2petclinic.services.CrudService;
+import guru.springframework.guruspring2petclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
