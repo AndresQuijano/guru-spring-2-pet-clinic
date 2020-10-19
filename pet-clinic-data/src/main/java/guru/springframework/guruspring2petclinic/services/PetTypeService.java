3a567d875451fba1e00b36cad720d28e0f8ae0d5
AndresQuijano <andresquijanoarroyave@gmail.com>
2020-10-18 23:24:09 -0500

    Lecture 121 done. Added PetTypeServiceMap implementation. Closes #29.

---
+++ pet-clinic-data/src/main/java/guru/springframework/guruspring2petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.guruspring2petclinic.services;
+
+import guru.springframework.guruspring2petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
