a4a1ae2079ba9d0bd5f5896a7f428946e83f38c9
AndresQuijano <andresquijanoarroyave@gmail.com>
2020-10-19 18:36:16 -0500

    Lecture 123 done. Added specialties map implementation. Closes #31.

---
+++ pet-clinic-data/src/main/java/guru/springframework/guruspring2petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.guruspring2petclinic.services;
+
+import guru.springframework.guruspring2petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty,Long> {
+}
