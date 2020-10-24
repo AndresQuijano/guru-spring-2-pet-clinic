3aed0761f611a3a9e7ad35742c8a82a60345297f
AndresQuijano <andresquijanoarroyave@gmail.com>
2020-10-24 10:49:06 -0500

    Lecture 156 done. Created Visit service and its map implementation. Closes #46.

---
+++ pet-clinic-data/src/main/java/guru/springframework/guruspring2petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.guruspring2petclinic.services;
+
+import guru.springframework.guruspring2petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
